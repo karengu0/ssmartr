fdede46bfd0ca5a4b9040346297b45c0418c38ae
Karen Guo <guokarennc@gmail.com>
2025-11-12 15:03:26 -0800

    Initial Commit

---
+++ ssmartr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ssmartr
+//
+//  Created by Karen Guo on 11/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ssmartr/ssmartrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ssmartrApp.swift
+//  ssmartr
+//
+//  Created by Karen Guo on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ssmartrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
